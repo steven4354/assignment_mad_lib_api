ff024135cc15ad82d8af1e617d5d94341f425947
Steven Li <steven4354@gmail.com>
2017-12-15 14:28:12 -0800

    fixing the madlib generator

--- routers/madlibs.js
+++ routers/madlibs.js
@@ -4,63 +4,62 @@ const helpers = require("./../helpers");
 const h = helpers.registered;
 const furiousSpinoff = require("furious_spinoff");
 const passport = require("passport");
-var Sentencer = require('sentencer');
 
 //do word
 var WordPOS = require("wordpos"),
   wordpos = new WordPOS();
+var Sentencer = require("sentencer");
 
 // ----------------------------------------
 // Index
 // ----------------------------------------
-router.get('/madlibgenerator', //?sentence=string&words=""
+router.post(
+  "/madlibgenerator", //no queries //post body sentence="" and words=[]
 
-passport.authenticate("bearer", {session: false}),
-  
+  passport.authenticate("bearer", {session: false}),
+
   async (req, res, next) => {
+    try {
+      let words = req.body.words.split(",") || "";
+      words = await wordpos.getPOS(words);
 
+      // {
+      //   nouns:[],       Array of words that are nouns
+      //   verbs:[],       Array of words that are verbs
+      //   adjectives:[],  Array of words that are adjectives
+      //   adverbs:[],     Array of words that are adverbs
+      //   rest:[]         Array of words that are not in dict or could not be categorized as a POS
+      // }
 
+      await Sentencer.configure({
+        // the list of nouns to use. Sentencer provides its own if you don't have one!
+        nounList: words.nouns,
 
-    try { 
-      let words = req.query.words.split(",");
-      
+        // the list of adjectives to use. Again, Sentencer comes with one!
+        adjectiveList: words.adjectives,
 
-      Sentencer.configure({
-  // the list of nouns to use. Sentencer provides its own if you don't have one! 
-  nounList: [],
- 
-  // the list of adjectives to use. Again, Sentencer comes with one! 
-  adjectiveList: [],
-  
-  //verbs
-  verbList: [],
+        //verbs
+        verbList: words.verbs,
 
-  //adverbs
+        //adverbs
 
-  adverbList: [],
-  // additional actions for the template engine to use. 
-  // you can also redefine the preset actions here if you need to. 
-  // See the "Add your own actions" section below. 
-  actions: {
-    my_action: function(){
-      return "something";
-    }
-  }
-});
+        adverbList: words.adverbs
+      });
 
+      let result = await Sentencer.make(req.body.sentence);
 
+      res.status(200).json(result);
 
-
+      //testing
+      /*
+      curl -X POST -H "Content-Type: application/json" -d '{Authorization: "Bearer 811adff41e1c1b4d887f7b9ba4f09a60", sentence: "This is {{ an_adjective }} sentence", words: [cool]}' http://127.0.0.1:3000/api/v1/madlibgenerator
+      curl -v -H "Authorization: Bearer 811adff41e1c1b4d887f7b9ba4f09a60" -H "Content-Type: application/json" -d '{sentence: "This is {{ an_adjective }} sentence", words: [cool]}' http://127.0.0.1:3000/api/v1/madlibgenerator
+      */
     } catch (e) {
       console.log(e);
     }
   }
 );
-
-
-
-
-
 
 router.get(
   "/partsofspeech", //?partofspeech=verb&num=10
@@ -76,28 +75,16 @@ router.get(
       const num = req.query.num || 10;
       const partofspeech = req.query.partofspeech || verb;
 
-      if (partofspeech === "verb"){
-      let result = await wordpos.randVerb({count: num});
-      } else if 
-         (partofspeech === "noun") {
-      let result = await wordpos.randNoun({count: num});
-          } else if 
-          (partofspeech === "adverb"){
-              let result = await wordpos.randAdverb({count: num});
-          } else if 
-          (partofspeech === "adjective"){
-              let result = await wordpos.randAdjective({count: num});
-          }
+      if (partofspeech === "verb") {
+        let result = await wordpos.randVerb({count: num});
+      } else if (partofspeech === "noun") {
+        let result = await wordpos.randNoun({count: num});
+      } else if (partofspeech === "adverb") {
+        let result = await wordpos.randAdverb({count: num});
+      } else if (partofspeech === "adjective") {
+        let result = await wordpos.randAdjective({count: num});
       }
 
-
-
-
-
-
-
-
-      console.log("result: " + result);
       res.status(200).json(result);
 
       //testing
